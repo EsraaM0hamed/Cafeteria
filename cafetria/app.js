de4cb8cd17aa2149568d089d719a14c146a713a3
kairo <kairo.wageh@yahoo.com>
2018-03-14 20:34:12 +0200

    forth

--- cafetria/app.js
+++ cafetria/app.js
@@ -1,31 +1,37 @@
+// import { Schema } from 'mongoose';
 
+// import { Server } from 'net';
 
 var express = require('express');
+
 var path = require('path');
+var flash =require('connect-flash');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
 var app = express();
 
 app.use(express.static('public'));
 var mongoose=require('mongoose');
 mongoose.connect("mongodb://localhost:27017/cafetria_db");
-console.log(mongoose);
-require('./models/orders');
+require('./models/products');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+app.use(flash()); 
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var products = require('./routes/product');
+
+
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
-var orders = require('./routes/orders');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -37,8 +43,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', index);
+
 app.use('/users', users);
-app.use('/orders',orders);
+app.use('/products', products);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -57,5 +64,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
 
 module.exports = app;
--- cafetria/routes/product.js
+++ cafetria/routes/product.js
@@ -4,44 +4,28 @@ var router =express.Router();
 var bodyParser=require('body-parser');
 var bodyParserMid=bodyParser.urlencoded();
 var multer=require('multer');
-
-
 var mongoose=require('mongoose');
 var uploadMid=multer({
     dest:"./public/imgs"
 });
-
-
 router.get('/add_product',function(req,resp){
 resp.render('product/add_product');
-
-
 });
-
-
 var productModel=mongoose.model('products');
-
 router.post('/add_product',uploadMid.single('product_img'),function(req,resp){
 //fs.renameSync(req.file.path,req.file.destination+"/"+req.file.originalname);
-   
    var product=new  productModel({
        p_name:req.body.product_name,
        p_price:req.body.product_price,
        p_category:req.body.product_category,
        p_img:req.file.filename,
    });
-   
      product.save(function(err,doc){
-
     console.log(err , doc);
     resp.json(doc);
    });
-
-    
 })
 router.get('/products',function(req,resp){
-   
-
 productModel.find({},function(err,result){
     if(!err){
         resp.render('product/products',{data:result});
@@ -49,13 +33,8 @@ productModel.find({},function(err,result){
     }else{
         resp.json(err);
     }
-
-})
-     
+})  
 });
-
 router.post('/products',bodyParserMid,function(req,resp){
-    
-
 });
 module.exports = router;
