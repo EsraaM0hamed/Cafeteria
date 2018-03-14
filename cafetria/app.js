a454173a081faf4fc1c873b586214b961f116746
nora <noramahmoudahmed9@gmail.com>
2018-03-14 22:49:03 +0200

    page of adding users and listing them

--- cafetria/app.js
+++ cafetria/app.js
@@ -5,11 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var app = express();
+var mongoose=require('mongoose');
+require('./models/users');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
-
+mongoose.connect("mongodb://localhost:27017/cafetria_db");
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
--- cafetria/models/users.js
+++ cafetria/models/users.js
@@ -1,23 +1,35 @@
+
 var mongoose=require('mongoose');
-var mongoosePaginate=require('mongoose-paginate');
+//var mongoosePaginate=require('mongoose-paginate');
 var Schema=mongoose.Schema;
+var imgPath = '1.png';
 var users=new Schema({
+   name:String,
+   email:String,
+   passw:String,
+   cpassw:String,
+   roomno:Number,
+   ext:Number,
+   img:String,
+
+/*
   _id:Number,
   name:String,
   dob:{
     type:Date,
     default:Date.now
   }
+*/
 });
 
-users.plugin(mongoosePaginate);
-
+//users.plugin(mongoosePaginate);
+/*
 
 users.post('remove',function(doc){
 var user_id=doc._id;
   mongoose.model("posts").remove({user:user_id},function(err,result){
     
   });
-});
+*/
 //register model 
 mongoose.model("users",users);
