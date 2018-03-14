07dbeedab11b3ce18e11a54b7dcd8ea2b9de5f4e
Esraa <esraa.mohameed@gmail.com>
2018-03-14 21:34:52 +0200

    connect add product with products and add product image

--- cafetria/models/products.js
+++ cafetria/models/products.js
@@ -9,5 +9,14 @@ var products=new Schema({
     p_img:String
    
    });
+   products.plugin(mongoosePaginate);
+   products.post('remove',function(doc){
+   var product_id=doc._id;
+   mongoose.model("products",remove({product:_id},function(err,result){
+
+
+   }))
+
+   })
   products.plugin(mongoosePaginate);
   mongoose.model("products",products);
--- cafetria/routes/product.js
+++ cafetria/routes/product.js
@@ -32,19 +32,62 @@ router.post('/add_product',uploadMid.single('product_img'),function(req,resp){
    });
    
      product.save(function(err,doc){
+         if(!err){
+             console.log("error");
+            //resp.render('product/products',{data:});
+           // resp.end();
+            resp.redirect('/products/products');
 
+         }else{
+            console.log("doc");
+
+             resp.json(err);
+         }
+   
     console.log(err , doc);
-    resp.json(doc);
+    //resp.json(doc);
    });
-
+  
     
+})
+
+router.get('/delete/:id',function(req,resp){
+    mongoose.model('[products]').remove({_id:req.params.id},function(err,result){
+        if(!err){
+            req.flash("msg","Done")
+            resp.redirect('/product/products');
+
+
+        }
+    })
+})
+
+
+
+
+router.get('/edit/:id',function(req,resp){
+    mongoose.model('products').findOne({_id:req.params.id},function (err,result) {
+        resp.render('product/edit',{obj:result});
+
+    })
+})
+router.post('/edit/:id',[bodyParserMid],function(req,resp){
+    console.log(req.params.id);
+    mongoose.model('products').update({_id:parseInt(req.params.id)},{
+
+    "$set":{title:req.body.title}
+    },function(err,doc){
+        
+        resp.redirect('/product/products');
+    });
 })
 router.get('/products',function(req,resp){
    
 
-productModel.find({},function(err,result){
+productModel.paginate({},{page:1,limit:25},function(err,result){
+
     if(!err){
-        resp.render('product/products',{data:result});
+        resp.render('product/products',{data:result.docs});
 
     }else{
         resp.json(err);
