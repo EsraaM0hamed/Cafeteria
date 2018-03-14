4a2647b02278ef51f94dcdc68c88cc2c58ea2884
abdelmun3m <abdelmunemelbasiouny@gmail.com>
2018-03-14 19:13:18 +0200

    add user make Order UI

---
+++ cafetria/public/javascripts/makeOrder.js
@@ -0,0 +1,55 @@
+
+var selectedProducts =[];
+ $(function () {
+
+   $(".product").click(function(){
+      $("#selected-products").append($(this).clone());
+
+      var u = {
+          img :this.children[0].attributes["src"].value,
+          id:this.children[1].innerText,
+          name:this.children[2].innerText,
+          price:this.children[3].innerText,
+      }
+      selectedProducts.push(u);
+      var s = "<div class='order-item'>"+
+        "<p class=''>"+u.name+"</p>"+
+        "<input class='' name='"+u.id+"' type='number' min='1' max='40' onchange='total()' value='1'/>"+
+        "<p class=''>EGP "+u.price+"</p>"+
+        "<button class='btn-cancel' type='button' name='button'>X</button>"+
+        "</div>";
+       $("#order-items").append(s);
+       total();
+   });
+
+
+   $("#order-items").click(function(event){
+     if(event.target.name=="button"){
+        event.target.parentNode.remove();
+        total();
+     }
+   });
+
+   $("#submit-order").click(function(event){
+      alert("submite !?");
+      //event.preventDefault();
+      //send request;
+
+   });
+
+
+
+});
+
+var total=function(){
+      var total = 0;
+    $(".order-item").each(function(e,product){
+
+          var y = new Number($(product.children[1]).val());
+          var x = new Number(product.children[2].innerText.split(" ")[1]);
+          if(y < 0) y = y*-1;
+          total +=y*x;
+          $("#total-order").text("EGP "+total);
+
+    })
+}
--- cafetria/routes/index.js
+++ cafetria/routes/index.js
@@ -3,7 +3,32 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+   var products = [
+    {id:1,name:"coca",img:"coca1.jpg",price:50},
+    {id:2,name:"pepesi",img:"pepsi.jpeg",price:10},
+    {id:3,name:"pepesi",img:"pepsi.jpeg",price:10},
+    {id:4,name:"coca",img:"coca1.jpg",price:50},
+    {id:5,name:"pepesi",img:"pepsi.jpeg",price:10},
+    {id:6,name:"coca",img:"coca1.jpg",price:50},
+    {id:7,name:"pepesi",img:"pepsi.jpeg",price:10},
+    {id:8,name:"coca",img:"coca1.jpg",price:50},
+    {id:9,name:"pepesi",img:"pepsi.jpeg",price:10},
+    {id:10,name:"coca",img:"coca1.jpg",price:50},
+    {id:11,name:"pepesi",img:"pepsi.jpeg",price:10},
+    {id:12,name:"coca",img:"coca1.jpg",price:50},
+    {id:13,name:"pepesi",img:"pepsi.jpeg",price:10},
+  ];
+  var rooms=[1,2,3,4,5,6];
+  //res.render('makeOrder',{products:products,rooms:rooms});
+  res.render('index',{products:products,rooms:rooms});
+
+});
+
+router.post("/sendOrder",function(req,res,next){
+
+
+  res.redirect('/');
 });
 
 module.exports = router;
