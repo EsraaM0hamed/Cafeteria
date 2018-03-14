13bdb4af098f532e4e9f2a02e76ae95e4e4331cf
kairo <kairo.wageh@yahoo.com>
2018-03-14 12:09:22 +0200

    first

--- cafetria/routes/orders.js
+++ cafetria/routes/orders.js
@@ -1,6 +1,12 @@
 var express = require('express');
-var router = express.Router();
+var fs=require('fs');
+var router=express.Router();
+var bodyParser=require('body-parser');
+var bodyParserMid=bodyParser.urlencoded();
+var multer=require('multer');
+var mongoose=require('mongoose');
 
+//var OrdersModel=mongoose.model('orders');
 /* GET orders listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -8,4 +14,14 @@ router.get('/', function(req, res, next) {
 router.get('/myorders', function(req, res, next) {
   res.render('orders/myorders');
 });
+//delete
+/*router.get('/delete/:id',function(req,resp){
+ 	OrdersModel.remove({_id:req.params.id},function(err,result){
+  if (!err) {
+    req.flash("msg","Done");
+    resp.redirect("/orders/myorders");
+
+  }
+})
+})*/
 module.exports = router;
