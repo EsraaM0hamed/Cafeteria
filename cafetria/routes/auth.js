c980a109ca0b5510cfb997578b857795eb987630
nora <noramahmoudahmed9@gmail.com>
2018-03-15 21:37:27 +0200

    athuntication of login

--- cafetria/routes/auth.js
+++ cafetria/routes/auth.js
@@ -1,26 +1,57 @@
-var express=require('express');
-var router=express.Router();
-var bodyParser = require('body-parser');
+var express = require('express');
+var router = express.Router();
+var bodyParser=require('body-parser');
 var bodyParserMid=bodyParser.urlencoded();
+var fs=require('fs');
+var multer=require('multer');
+var mongoose=require('mongoose');
+
+
+var user=mongoose.model('users');
+
 router.get('/login',function(req,resp){
     resp.render("auth/login",{
         message:req.flash("msg")
     });
 });
 router.post('/login',bodyParserMid,function(req,resp){
+
+
     var email=req.body.email;
     var password=req.body.password;
-    if(email=="n@n.com"&&password.valueOf()=="123"){
-        req.session.email="n@n.com";
-        req.session.password="123";
-        resp.redirect("/users/list");
-    }else{
-        req.flash("msg","invalid email & password");
-        resp.redirect("../users/add");
-    }
-});
+    user.findOne({ email: email ,passw:password},function(err,result){
+        if(err){
+            resp.redirect('/auth/login');
+        }
+        if(email=="" || password==""){
+            console.log("he");
+            req.flash("msg",'invalide email');
+            resp.redirect('/auth/login');
+
+        }
+        if(result != null){
+            
+             if(email == result.email && password == result.passw ){
+              console.log("hi");
+                req.session.email=email ;
+                req.session.password = password;
+                resp.redirect("../users/list");
+                }
+                else{
+                    req.flash("msg",'invalide email');
+                    resp.redirect('/auth/login');
+                }
+            }
+            else{
+                resp.redirect('/auth/login');
+            }
+
+
 
 module.exports=router;
+
+
+
 
 
 
